8071236e5a68b465f481331e0f85baa507b200cb
Poem Liu <lovececilia@icloud.com>
2025-02-06 19:10:15 +0800

    nianniancard

---
+++ app/ui/nianniancard.tsx
@@ -0,0 +1,11 @@
+import { Card, CardBody } from "@heroui/react";
+
+export default function LittleCuteCard() {
+  return (
+    <Card>
+      <CardBody>
+        <p>念念</p>
+      </CardBody>
+    </Card>
+  );
+}
